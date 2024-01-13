1320db2e0d16f8c379c0426a15e353752d0936b9
Python-Freak <mihirom9876@gmail.com>
2024-01-13 16:52:48 +0530

    Events Done + Changes in Navbar

---
+++ client/src/Components/EventCard/EventCard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from "./EventCard.module.css"
+import "./card.css"
+import Button from 'react-bootstrap/Button';
+
+function EventCard(props) {
+    return (
+        <div className="card card-product">
+            <div className="card-header card-header-image bg-white border-0" data-header-animation="true">
+                <img src="/assets/images/events/sample_event.jpeg" className="img-fluid" />
+            </div>
+            <div className="card-body">
+                <h4 className="card-title text-center">
+                    <a href="#" title="Toyota" className={`text-decoration-none title__`} >{props.eventName}</a>
+                </h4>
+                <div className="card-description text-center">
+                    {props.description}
+                </div>
+            </div>
+            <div className="card-footer">
+                <div className="price">
+                    <h4 className="p-0 m-0">{props.eventDate}</h4>
+                </div>
+                <div className="stats">
+                    <p className="card-category p-0 m-0"><Button href={`${props.regLink}`}>Register</Button></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EventCard
--- client/src/Components/Navbar__/Navbar__.js
+++ client/src/Components/Navbar__/Navbar__.js
@@ -3,37 +3,50 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import styles from "./Navbar.module.css"
+import { useState } from 'react';
 
 function Navbar__() {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleToggle = () => {
+        setIsExpanded(!isExpanded);
+    }
+
     return (
-        <Navbar bg="dark" expand="lg" variant="dark" className={`bg-body-tertiary ${styles.navbar__}`}>
-            <Navbar.Brand href="/">
-                <div className="row">
-                    <div className="col">
-                        <img className={styles.logo} src="/assets/images/CSI_LOGO.png" alt="CSI LOGO" />
-                    </div>
-                    <div className="col" style={{ marginLeft: 20 }}>
+        <>
+            <Navbar bg="dark" expand="lg" variant="dark" className={`bg-body-tertiary ${styles.navbar__}`}>
+                <Navbar.Brand href="/">
+                    <div className="row">
                         <div className="col">
-                            <div className="row" style={{ color: "white", marginTop: 2 }}>
-                                Computer Society of India
-                            </div>
-                            <div className={`row ${styles.small_font}`}>
-                                Nirma University Student Chapter
-                            </div>
+                            <img className={styles.logo} src="/assets/images/CSI_LOGO.png" alt="CSI LOGO" />
                         </div>
+                        <div className="col" style={{ marginLeft: 20 }}>
+                            <div className="col">
+                                <div className={`row ${styles.large_font}`} style={{ color: "white", marginTop: 5 }}>
+                                    Computer Society of India
+                                </div>
+                                <div className={`row ${styles.small_font}`}>
+                                    Nirma University Student Chapter
+                                </div>
+                            </div>
 
+                        </div>
                     </div>
-                </div>
-            </Navbar.Brand >
+                </Navbar.Brand >
 
-            <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ marginRight: 5 }} />
-            <Navbar.Collapse id="basic-navbar-nav" className={styles.right}>
-                <Nav.Link href="#home" className={styles.nav_links}>Events</Nav.Link>
-                <Nav.Link href="#link" className={styles.nav_links}>Blogs</Nav.Link>
-                <Nav.Link href="#home" className={styles.nav_links}>Contact Us</Nav.Link>
-                <Nav.Link href="#link" className={styles.nav_links}>About Us</Nav.Link>
-            </Navbar.Collapse>
-        </Navbar >
+                <Navbar.Toggle aria-controls={`basic-navbar-nav`} style={{
+                    marginRight: 15
+                }} className={`${styles.no_shadow} ` + (isExpanded ? `${styles.expand_shadow}` : '')} onClick={handleToggle} />
+                <Navbar.Collapse id="basic-navbar-nav" className={styles.right}>
+                    <Nav.Link href="#home" className={styles.nav_links} >Events</Nav.Link>
+                    <Nav.Link href="#link" className={styles.nav_links} >Blogs</Nav.Link>
+                    <Nav.Link href="#home" className={styles.nav_links} >Contact Us</Nav.Link>
+                    <Nav.Link href="#link" className={styles.nav_links} >About Us</Nav.Link>
+                </Navbar.Collapse>
+            </Navbar >
+
+            <div className={`${styles.nav_divider}`}></div>
+        </>
     );
 }
 
