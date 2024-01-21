b4e5ed7f8f873142079a9d451a4c70584af9d68c
Python-Freak <mihirom9876@gmail.com>
2024-01-21 13:52:12 +0530

    TESTING FILES

--- server/server/settings.py
+++ server/server/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'test',
+    'test',
     'authentication',
     'rest_framework',
     'djoser',
